f277ad9ed8e5242767823e771dc8ddf068f8ba10
grud <nggridteam@gmail.com>
2020-12-01 12:52:20 +0000

    v4.9.2

--- ui-grid.js
+++ ui-grid.js
@@ -1,5 +1,5 @@
 /*!
- * ui-grid - v4.9.1 - 2020-10-26
+ * ui-grid - v4.9.2 - 2020-12-01
  * Copyright (c) 2020 ; License: MIT 
  */
 
@@ -28958,6 +28958,7 @@ module.filter('px', function() {
        */
       createTree: function( grid, renderableRows ) {
         var currentLevel = -1,
+          parentsCache = {},
           parents = [],
           currentState;
 
@@ -28988,16 +28989,31 @@ module.filter('px', function() {
             }
           }
 
+          // If row header as parent exists in parentsCache
+          if (
+            typeof row.treeLevel !== 'undefined' &&
+            row.treeLevel !== null &&
+            row.treeLevel >= 0 &&
+            parentsCache.hasOwnProperty(row.uid)
+          ) {
+            parents.push(parentsCache[row.uid]);
+          }
+
           // aggregate if this is a leaf node
           if ( ( typeof(row.treeLevel) === 'undefined' || row.treeLevel === null || row.treeLevel < 0 ) && row.visible  ) {
             service.aggregate( grid, row, parents );
           }
 
           // add this node to the tree
-          service.addOrUseNode(grid, row, parents, aggregations);
+          if (!parentsCache.hasOwnProperty(row.uid)) {
+            service.addOrUseNode(grid, row, parents, aggregations);
+          }
 
           if ( typeof(row.treeLevel) !== 'undefined' && row.treeLevel !== null && row.treeLevel >= 0 ) {
-            parents.push(row);
+            if (!parentsCache.hasOwnProperty(row.uid)) {
+              parentsCache[row.uid] = row;
+              parents.push(row);
+            }
             currentLevel++;
             currentState = service.setCurrentState(parents);
           }
